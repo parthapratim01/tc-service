6010631b685c4b17e0021168d2a02c1c7e6c4a43
Partha Pratim Baral <parthapratim.baral@yahoo.in>
2024-02-18 23:18:01 +0530

    Initial commit

---
+++ src/main/java/com/tc/collabapi/CollabapiApplication.java
@@ -0,0 +1,14 @@
+package com.tc.collabapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabapiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CollabapiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tc/collabapi/controller/CollaborationController.java
@@ -0,0 +1,27 @@
+package com.tc.collabapi.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User: Partha Pratim Baral
+ * Topic :
+ * Date: 2/18/2024
+ */
+
+@RestController
+@RequestMapping("/v1")
+public class CollaborationController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CollaborationController.class);
+
+    @GetMapping("/ping")
+    public String pingService() {
+
+        logger.info(" PingService method call");
+        return "Collaboration API is Up and running";
+    }
+}
